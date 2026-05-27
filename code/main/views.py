2b9dd565ecdae4fec7df2b2e6e8dfa7605259987
Jerry-create-ive <yinhongzhaoxue@qq.com>
2026-05-27 11:11:08 +0800

    feat: 添加图片测试页面与配套静态资源
    新增了图片加载测试页面test_images.html，添加了16张静态测试图片，同时新增了测试路由用于访问该页面

--- code/main/views.py
+++ code/main/views.py
@@ -2,3 +2,6 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'index.html')
+
+def test_images(request):
+    return render(request, 'test_images.html')
--- code/red_story/urls.py
+++ code/red_story/urls.py
@@ -5,4 +5,5 @@ from main import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.index, name='index'),
+    path('test/', views.test_images, name='test_images'),
 ]
